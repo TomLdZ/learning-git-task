cc436a72ca8b828da87493162abeea202ed80b4c
Tomasz Łydziński <tomasz.lydzinski33@gmail.com>
2024-07-24 09:45:12 +0200

    Add last part of raport, join and print raport

--- lista_zakupów_task.py
+++ lista_zakupów_task.py
@@ -10,3 +10,9 @@ for shop in shopping_dict:
     list_capitalized = list(item.capitalize() for item in shopping_dict[shop])
     raport_parts.append(f"Idę do {shop.capitalize()}, kupuję tu następujące rzeczy: {list_capitalized}")
     prod_quantity += len(list_capitalized)
+
+raport_parts.append(f"W sumie kupuję {prod_quantity} produktów")
+
+raport = "\n".join(raport_parts)
+
+print(raport)
