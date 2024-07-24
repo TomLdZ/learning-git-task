7b767060953680d0b566b2c9589acca06c51dd27
Tomasz Łydziński <tomasz.lydzinski33@gmail.com>
2024-07-24 09:38:11 +0200

    Add 1 part of raport and var quantity

--- lista_zakupów_task.py
+++ lista_zakupów_task.py
@@ -1,2 +1,8 @@
 shopping_dict = {"piekarnia":["chleb", "pączek", "bułki"], "warzywniak":["marchew", "seler", "rukola"]}
 
+prod_quantity = 0
+
+raport_parts = []
+
+raport_parts.append("Lista zakupów:")
+
